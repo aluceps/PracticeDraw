cc9a7f605914ca09a9159924740142ee4c4632f4
Atsushi Takano <aluceps@gmail.com>
2019-02-19 12:31:46 +0900

    first commit

---
+++ app/src/main/java/me/aluceps/practicedraw/MainActivity.kt
@@ -0,0 +1,24 @@
+package me.aluceps.practicedraw
+
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.aluceps.practicedraw.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupClick()
+    }
+
+    private fun setupClick() {
+        binding.toolbar.undo.setOnClickListener { }
+        binding.toolbar.redo.setOnClickListener { }
+        binding.toolbar.reset.setOnClickListener { }
+    }
+}
